bee252bb7fe2df73a0243818a5b7ad347c34f52c
miilljana <67390212+miilljana@users.noreply.github.com>
2022-08-29 10:51:50 +0200

    add emission vehicles

--- src/main/java/org/matsim/project/CreateEmissionVehicles.java
+++ src/main/java/org/matsim/project/CreateEmissionVehicles.java
@@ -2,42 +2,59 @@ package org.matsim.project;
 
 import org.matsim.api.core.v01.Id;
 import org.matsim.api.core.v01.Scenario;
-import org.matsim.api.core.v01.population.Person;
+import org.matsim.core.config.Config;
 import org.matsim.core.config.ConfigUtils;
-import org.matsim.core.population.io.PopulationReader;
 import org.matsim.core.scenario.ScenarioUtils;
 import org.matsim.vehicles.*;
 
-import java.util.Random;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Map;
+
 
 public class CreateEmissionVehicles {
-    public static void run(String	inputPoopulationFilename,	String	outputVehiclesFilename) {
-        Scenario scenario	=	ScenarioUtils.createScenario(ConfigUtils.createConfig());
-        new PopulationReader(scenario).readFile(inputPoopulationFilename);
-        Vehicles vehicles	= scenario.getVehicles();
-        VehiclesFactory factory	=	vehicles.getFactory();
-        VehicleType avgCarType	=	factory.createVehicleType(Id.create("car_average",	VehicleType.class));
-        avgCarType.setDescription("BEGIN_EMISSIONSPASSENGER_CAR;average;average;averageEND_EMISSIONS");
-        vehicles.addVehicleType(avgCarType);
-        VehicleType	petrolCarType	=	factory.createVehicleType(Id.create("car_petrol",	VehicleType.class));
-        petrolCarType.setDescription("BEGIN_EMISSIONSPASSENGER_CAR;petrol	(4S);>=2L;PC-P-Euro-1END_EMISSIONS");
-        vehicles.addVehicleType(petrolCarType);
-        Random r	=	new	Random(123456);
-        for	(Person person	:	scenario.getPopulation().getPersons().values())	{
-            VehicleType	vehType	=	avgCarType;
-            if	(r.nextDouble()	<	0.5)	{
-                vehType	=	petrolCarType;
-            }
-            Vehicle vehicle	=	factory.createVehicle(Id.create(person.getId().toString(),	Vehicle.class),	vehType);
-            vehicles.addVehicle(vehicle);
+
+    public static void run(Path results_path, String all_vehicles, String output_emission_vehicles){
+
+        Config config = ConfigUtils.loadConfig(Paths.get(results_path.getParent().getParent().toString(),"config.xml").toString());
+
+        config.vehicles().setVehiclesFile(all_vehicles);
+        Scenario scenario = ScenarioUtils.loadScenario(config);
+
+        Vehicles vehiclesHbefa = VehicleUtils.createVehiclesContainer();
+        VehiclesFactory factory = vehiclesHbefa.getFactory();
+
+        VehicleType bicycleType =  factory.createVehicleType(Id.create("bicycle",VehicleType.class));
+//        bicycleType.setDescription("BEGIN_EMISSIONSPASSENGER_CAR;average;average;averageEND_EMISSIONS");
+        bicycleType.getEngineInformation().getAttributes().putAttribute("HbefaVehicleCategory","NON_HBEFA_VEHICLE");
+
+        vehiclesHbefa.addVehicleType(bicycleType);
+
+        VehicleType carType = factory.createVehicleType(Id.create("car",VehicleType.class));
+//        carType.setDescription("BEGIN_EMISSIONSPASSENGER_CAR;average;average;averageEND_EMISSIONS");
+        carType.getEngineInformation().getAttributes().putAttribute("HbefaVehicleCategory","PASSENGER_CAR");
+        carType.getEngineInformation().getAttributes().putAttribute("HbefaTechnology","average");
+        carType.getEngineInformation().getAttributes().putAttribute("HbefaSizeClass","average");
+        carType.getEngineInformation().getAttributes().putAttribute("HbefaEmissionsConcept","average");
+        vehiclesHbefa.addVehicleType(carType);
+
+        VehicleType busType = factory.createVehicleType(Id.create("bus",VehicleType.class));
+//        busType.setDescription("BEGIN_EMISSIONSPASSENGER_CAR;average;average;averageEND_EMISSIONS");
+        busType.getEngineInformation().getAttributes().putAttribute("HbefaVehicleCategory","PASSENGER_CAR");
+        busType.getEngineInformation().getAttributes().putAttribute("HbefaTechnology","average");
+        busType.getEngineInformation().getAttributes().putAttribute("HbefaSizeClass","average");
+        busType.getEngineInformation().getAttributes().putAttribute("HbefaEmissionsConcept","average");
+        vehiclesHbefa.addVehicleType(busType);
+
+        Map<Id<Vehicle>, Vehicle> vehicles = scenario.getVehicles().getVehicles();
+
+
+        for (Id<Vehicle> v: vehicles.keySet()){
+            Vehicle vehicle = factory.createVehicle(v,vehicles.get(v).getType());
+            vehiclesHbefa.addVehicle(vehicle);
         }
-        new MatsimVehicleWriter(vehicles).writeFile(outputVehiclesFilename);
 
-    }
+        new MatsimVehicleWriter(vehiclesHbefa).writeFile(output_emission_vehicles);
 
-    public static void main(String[] args) {
-        String populationFileName = "Scenarios/BilbaoTimeVariantNetwork/input/population_10Percents.xml";
-        String outputVehiclesFileName = "Scenarios/BilbaoTimeVariantNetwork/inputEmissions/vehicles.xml";
-        run(populationFileName,outputVehiclesFileName);
     }
 }
